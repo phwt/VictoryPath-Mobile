ebeccbf2f97c9a9c857246fbe93b0deb6404de13
jfornqz <42958376+jfornqz@users.noreply.github.com>
2020-11-22 23:16:17 +0700

    loop grid โง่ๆ

--- screen/Mode/easy.js
+++ screen/Mode/easy.js
@@ -34,107 +34,101 @@ import { TouchableOpacity } from "react-native-gesture-handler";
 
 
 export default function Easy(props) {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.header}>Easy Mode</Text>
-      <View style={styles.frame}>
-        <View style={styles.gridlayout}>
+    var i;
+    var j;
+    var row = [];
+    var col = [];
+    for (j = 0; j < 4; j++) {
+        col.push(
             <TouchableOpacity style={styles.grid} />
-            <TouchableOpacity style={styles.grid} />
-            <TouchableOpacity style={styles.grid} />
-            <TouchableOpacity style={styles.grid} />
+        )
+    }
+    for (i = 0; i < 4; i++) {
+        row.push(
+            <View style={styles.gridlayout}>{col.map((item) => item)}</View>
+        )
+    }
+    
+    // console.log(col);
+    return (
+        <View style={styles.container}>
+            <Text style={styles.header}>Easy Mode</Text>
+            <View style={styles.frame}>
+                {row.map((item) => item)}
+            </View>
+            <View style={styles.btnrow}>
+                <TouchableOpacity style={styles.btngreen}
+                    onPress={() => {
+                        props.navigation.navigate("GameModeScreen");
+                    }}
+                >
+                    <Text style={styles.text}>select mode</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.btnred}>
+                    <Text style={styles.text}>End game</Text>
+                </TouchableOpacity>
+            </View>
         </View>
-        <View style={styles.gridlayout}>
-            <TouchableOpacity style={styles.grid} />
-            <TouchableOpacity style={styles.grid} />
-            <TouchableOpacity style={styles.grid} />
-            <TouchableOpacity style={styles.grid} />
-        </View>
-        <View style={styles.gridlayout}>
-            <TouchableOpacity style={styles.grid} />
-            <TouchableOpacity style={styles.grid} />
-            <TouchableOpacity style={styles.grid} />
-            <TouchableOpacity style={styles.grid} />
-        </View>
-        <View style={styles.gridlayout}>
-            <TouchableOpacity style={styles.grid} />
-            <TouchableOpacity style={styles.grid} />
-            <TouchableOpacity style={styles.grid} />
-            <TouchableOpacity style={styles.grid} />
-        </View>
-      </View>
-      <View style={styles.btnrow}>
-        <TouchableOpacity style={styles.btngreen}
-            onPress={() => {
-            props.navigation.navigate("GameModeScreen");
-          }}
-        >
-            <Text style={styles.text}>select mode</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.btnred}>
-            <Text style={styles.text}>End game</Text>
-        </TouchableOpacity>
-      </View>
-    </View>
-  );
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#000",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  frame:{
-    
-  },
-  header: {
-    color: "white",
-    fontWeight: "bold",
-    fontSize: 25,
-    marginBottom: 50,
-  },
-  gridlayout: {
-    flexDirection: "row",
-    
+    container: {
+        flex: 1,
+        backgroundColor: "#000",
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    frame: {
 
-  },
-  grid:{
-    margin: "0.5%",
-    width: 80,
-    height: 80,
-    backgroundColor: '#F8F9F9',
-    borderRadius: 30,
-  },
-  btngreen:{
-    margin: 25,
-    borderRadius: 15,
-    backgroundColor: "green",
-    alignItems: "center",
-    height: 50,
-    width: 120,
-  },
-  btnred:{
-    margin: 25,
-    borderRadius: 15,
+    },
+    header: {
+        color: "white",
+        fontWeight: "bold",
+        fontSize: 25,
+        marginBottom: 50,
+    },
+    gridlayout: {
+        flexDirection: "row",
 
-    backgroundColor: "red",
-    alignItems: "center",
-    height: 50,
-    width: 120,
-  },
 
-  text:{
-    fontSize: 18,
-    color: "white",
-    fontStyle: "italic",
-    marginTop: 10,
+    },
+    grid: {
+        margin: "0.5%",
+        width: 75,
+        height: 75,
+        backgroundColor: '#F8F9F9',
+        borderRadius: 30,
+    },
 
-  },
-  btnrow:{
-    flexDirection: "row",
-    marginTop: 40,
-  }
+    btngreen: {
+        margin: 25,
+        borderRadius: 15,
+        backgroundColor: "green",
+        alignItems: "center",
+        height: 50,
+        width: 120,
+    },
+    btnred: {
+        margin: 25,
+        borderRadius: 15,
+
+        backgroundColor: "red",
+        alignItems: "center",
+        height: 50,
+        width: 120,
+    },
+
+    text: {
+        fontSize: 18,
+        color: "white",
+        fontStyle: "italic",
+        marginTop: 10,
+
+    },
+    btnrow: {
+        flexDirection: "row",
+        marginTop: 40,
+    }
 
 });
