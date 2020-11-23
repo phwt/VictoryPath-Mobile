01d7d57035afa45aa908bedd03bb8b5ebf5ee8a9
jfornqz <42958376+jfornqz@users.noreply.github.com>
2020-11-24 04:05:01 +0700

    new grid table

--- screen/Mode/easy.js
+++ screen/Mode/easy.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Text, View, StyleSheet } from "react-native";
 import { TouchableOpacity } from "react-native-gesture-handler";
+// import BgAudio from 'react-native-background-audio'
 
 // const Width = 10;
 // const Height = 10;
@@ -31,30 +32,115 @@ import { TouchableOpacity } from "react-native-gesture-handler";
 //     }
 // }
 
-
+// const audio_options = {
+//     source:{local: require('../assets/bgmusic.mp3')} 
+//   }
 
 export default function Easy(props) {
-    var i;
-    var j;
-    var row = [];
-    var col = [];
-    for (j = 0; j < 4; j++) {
-        col.push(
-            <TouchableOpacity style={styles.grid} />
-        )
-    }
-    for (i = 0; i < 4; i++) {
-        row.push(
-            <View style={styles.gridlayout}>{col.map((item) => item)}</View>
-        )
+    // var i;
+    // var j;
+    // var k;
+    // var row = [];
+    // var col = [];
+    // var colkey = 0;
+    // var rowkey = 0;
+    // var allcolor = ["#2fc1ce", "#ffb300", "#ff1280", "#ca8dc9", "#f9ec00", "#6f369d"];
+    // var danger = "#000000";
+    // var randomColor = require('randomcolor'); // import the script
+    // var color = randomColor(); // a hex code for an attractive color
+
+    let rows = []
+    let nums = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16]]
+
+    for (let i = 0; i < 4; i++) {
+      let row = []
+      for (let j = 0; j < 4; j++) {
+        row.push(<TouchableOpacity style={styles.grid} onPress={() => this.buttonPress(nums[i][j])}>
+          <Text style={{color:"black", fontSize:40}}>{nums[i][j]}</Text>
+        </TouchableOpacity>)
+      }
+      rows.push(<View style={styles.gridlayout}>{row}</View>)
     }
     
-    // console.log(col);
+    // for (j = 0; j < 4; j++) {
+        
+    //     colkey+=1;
+    //     col.push(
+    //         <TouchableOpacity key={colkey} style={styles.grid} >
+    //             <Text style={{color:"black", fontSize:40}}>
+    //                 {colkey}
+    //             </Text>
+    //         </TouchableOpacity>
+            
+    //     );
+        
+        
+    // }
+    // for (i = 0; i < 4; i++) {
+
+    //     row.push(
+    //         <View key={rowkey} style={styles.gridlayout}>{col.map((item) => item)}</View>
+    //     )
+    //     rowkey +=1;
+    //     console.log(rowkey,colkey)
+    // }
+    // console.log("---","row")
+
+    // function resetGrid() {
+
+    //     grid = [];
+      
+    //     tileSize = Math.min(width / gridSize, height / gridSize);
+      
+    //       /* creates 2D array */
+    //     for (var x = 0; x < gridSize; x++) {
+    //       var col = [];
+    //       for (var y = 0; y < gridSize; y++) {
+      
+    //         col.push(new Tile(x, y, false));
+    //       }
+    //       grid.push(col);
+    //     }
+      
+    //   };
+    // <Text style={{color:"black", fontSize:35}}>{testNum}</Text>
+    // for (i = 0; i < 4; i++) {
+        
+    //     row.push(
+    //         <View key={rowkey} style={styles.gridlayout}>{col.map((item) => item)}</View>
+    //     )
+    //     rowkey +=1;
+    //     console.log(rowkey,"row")
+    // }
+    // for (j = 0; j < 4; j++) {
+    //     testNum+=1
+    //     col.push(
+    //         <Text style={{color:"white"}}>{testNum}</Text>
+             
+    //     )
+        
+    // }
+    // col.push(
+    //     <Text style={{color:"white"}}>+</Text>
+        
+        
+    // )
+    //  console.log(col[1])
+    // for (i = 0; i < 4; i++) {
+    //     row.push(
+    //         <View style={styles.gridlayout}>{col.map((item) => item)}</View>
+    //     )
+    // }
+
+    //   console.log(row[1]);
     return (
         <View style={styles.container}>
+        {/* <BgAudio options={audio_options}></BgAudio> */}
             <Text style={styles.header}>Easy Mode</Text>
             <View style={styles.frame}>
-                {row.map((item) => item)}
+            {/* <Text>{row.map((item) => item)}</Text> */}
+                {/* {row.map((item) => item)} */}
+                {rows}
             </View>
             <View style={styles.btnrow}>
                 <TouchableOpacity style={styles.btngreen}
@@ -97,8 +183,9 @@ const styles = StyleSheet.create({
         margin: "0.5%",
         width: 75,
         height: 75,
-        backgroundColor: '#F8F9F9',
+        backgroundColor: 'red',
         borderRadius: 30,
+        // '#F8F9F9'
     },
 
     btngreen: {
