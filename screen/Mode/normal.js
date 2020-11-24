3db87f361dd081c5bb50f96c5b5d0a2c8eb1af06
Thitiwut Tosanguan <l3luehunter@gmail.com>
2020-11-25 06:58:17 +0700

    game

--- screen/Mode/normal.js
+++ screen/Mode/normal.js
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from "react";
-import { View, Button, StyleSheet, TouchableOpacity, Text } from "react-native";
+import { View, Button, StyleSheet, TouchableOpacity, Text , Alert} from "react-native";
 
 
 export default function Normal(props) {
     const [activeColor, setActiveColor] = useState("#40FFFF");
+    const [gameplay, setGamePlay] = useState(false);
+    const [score, setScore] = useState(0);
+    const [pathList, setPathList] = useState([])
 
     //Customize Button
     const PathButton = ({onPress}) => (<TouchableOpacity onPress={onPress} style={{margin: "0.5%",
@@ -12,78 +15,98 @@ export default function Normal(props) {
     backgroundColor: activeColor,
     borderRadius: 30}}/>);
     const WrongButton = ({onPress}) => (<TouchableOpacity onPress={onPress} style={styles.grid}/>);
-
+    if (gameplay)
     var timedisplay = 3000; //เวลาที่จะแสดงสีค้างไว้
     var size = 5; //ไซส์ของตาราง
-    var score = 0; //คะแนน
+    // var score = 0; //คะแนน
     var click = 0; //กดไปกี่ครั้ง
     var rows = [];
-    var result = [];
     var buttonValue = [[1,2,3,4,5],
                         [6,7,8,9,10],
                         [11,12,13,14,15],
                         [16,17,18,19,20],
                         [21,22,23,24,25]];
-
     //เช็คคำตอบ
     function addToAnswer(number){
-        console.log(number);
-        if (number == result[click]){
-            click += 1;
-            console.log(number+" Nice bro!!!");
-            if (click == size){
-                score += 1;
-                console.log("***** Now your score is: " +'"'+ yourScore + '"'+ " ******");
-                click = 0;
-                createGrid();
+        if (activeColor == "#7F8080"){
+            if (number == pathList[click]){
+                click += 1;
+                console.log(number+" Nice bro!!!");
+                if (click == size){
+                    setScore(score+1);
+                    click = 0;
+                    console.log(gameplay);
+                    setGamePlay(false);
+                    console.log("endgame:"+gameplay);
+                    pathList.splice(0, size);
+                    console.log("result at end: "+pathList);
+                    rows.splice(0, size);
+                    setActiveColor("#40FFFF");
+                }
+            }else{
+                console.log("Defeat!!!");
+                Alert.alert(
+                    "Defeat!",
+                    "Your score is "+ score,
+                    [
+                      { text: "OK", onPress: () => props.navigation.navigate("GameModeScreen")}
+                    ],
+                    { cancelable: false }
+                  );
+                // Alert.alert("Defeat! your score is " + score);
+                
             }
-        }else{
-            console.log("Defeat!!!");
-            props.navigation.navigate("GameModeScreen");
         }
     }
 
     function createGrid(){
-        //สุ่มตัวเลข ตำแหน่ง Path
+        if (gameplay==false){
         var temp = 100;
-        for (let i = 0, j = size - 1; i < size; i++, j--) {
-            if (temp != 100) {
-                var combo = Math.floor(Math.random() * 5);
-                if (combo === 0 && temp != 0)
-                    var pos = temp - 1;
-                else if (combo === 1 && temp != 0)
-                    var pos = temp - 1;
-                else if (combo === 2)
-                    var pos = temp;
-                else if (combo === 3 && temp != (size - 1))
-                    var pos = temp + 1;
-                else if (combo === 4 && temp != (size - 1))
-                    var pos = temp + 1;
-            } else
-                pos = Math.floor(Math.random() * size);
-            temp = pos;
-            result.push(buttonValue[j][pos]);
+            for (let i = 0, j = size - 1; i < size; i++, j--) {
+                if (temp != 100) {
+                    var combo = Math.floor(Math.random() * 5);
+                    if (combo === 0 && temp != 0)
+                        var pos = temp - 1;
+                    else if (combo === 1 && temp != 0)
+                        var pos = temp - 1;
+                    else if (combo === 2)
+                        var pos = temp;
+                    else if (combo === 3 && temp != (size - 1))
+                        var pos = temp + 1;
+                    else if (combo === 4 && temp != (size - 1))
+                        var pos = temp + 1;
+                } else
+                    pos = Math.floor(Math.random() * size);
+                temp = pos;
+                pathList.push(buttonValue[j][pos]);
+            }
+            console.log("gameplay:"+gameplay);
+            console.log("result: "+pathList);
+            setGamePlay(true);
+            // setPathList([]);
+            // setGamePlay(false);
+            // console.log("result: "+pathList);
+        //สร้าง Grid            
         }
-        console.log("result: "+result);
-
         for (let i = 0; i < size; i++) {
             let row = [];
             for (let j = 0; j < size; j++) {
-                if(result.includes(buttonValue[i][j])){
+                if(pathList.includes(buttonValue[i][j])){
                     row.push(<PathButton onPress={()=>addToAnswer(buttonValue[i][j])}/>);
                 }else{
                     row.push(<WrongButton onPress={()=>addToAnswer(buttonValue[i][j])}/>);
                   } 
             }
             rows.push(<View style={styles.gridlayout}>{row}</View>);
-        }
-        setTimeout(function(){
-            setActiveColor("#7F8080");//เปลี่ยน Path กลับเป็นสีเทา
-        }, timedisplay);
-    }
-    
+        }}
 
-    createGrid();
+        useEffect(() => {
+            const toggle = setTimeout(function(){
+                setActiveColor("#7F8080");//เปลี่ยน Path กลับเป็นสีเทา
+            }, timedisplay);
+        })
+
+        createGrid();
     return (
         <View style={styles.container}>
             <Text style={styles.header}>Normal Mode</Text>
@@ -95,7 +118,9 @@ export default function Normal(props) {
                 <TouchableOpacity style={styles.btngreen} onPress={() => {props.navigation.navigate("GameModeScreen");}}>
                     <Text style={styles.text}>select mode</Text>
                 </TouchableOpacity>
-                <TouchableOpacity style={styles.btnred}>
+                <TouchableOpacity style={styles.btnred} onPress={() => {
+            props.navigation.navigate("GameScreen");
+          }}>
                     <Text style={styles.text}>End game</Text>
                 </TouchableOpacity>
             </View>
